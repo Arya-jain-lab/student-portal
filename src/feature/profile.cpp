d8ebdc95cbd1dd4edce8e72bbb40400dc672f8ce
Arya Jain <arya.jain@students.iiit.ac.in>
2026-08-15 20:03:55 +0530

    Add display profile function

--- src/feature/profile.cpp
+++ src/feature/profile.cpp
@@ -5,3 +5,7 @@ struct UserProfile {
     std::string name;
     std::string email;
 };
+
+void displayProfile(const UserProfile& profile) {
+    std::cout << "Name: " << profile.name << ", Email: " << profile.email << std::endl;
+}
